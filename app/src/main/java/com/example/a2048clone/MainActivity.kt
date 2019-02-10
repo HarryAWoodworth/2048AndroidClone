a6013d8dc14984e85966ee61d0475784c5c5d832
Harry Woodworth <harryawoodworth@gmail.com>
2019-02-10 18:22:35 -0500

    Add Initial UI

---
+++ app/src/main/java/com/example/a2048clone/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.a2048clone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Hide action bar
+        supportActionBar?.hide()
+
+        // Hide the UI
+        hideUI()
+    }
+
+    // Activate immersive mode through flags
+    private fun hideUI()
+    {
+        // Immersive mode
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+}
